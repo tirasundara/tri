c3d354b4b6601945671e67017e9b85349faac688
Tira Sundara <sundaralinus@gmail.com>
2021-11-02 19:24:42 +0700

    Update the Long description

--- cmd/root.go
+++ cmd/root.go
@@ -18,6 +18,7 @@ package cmd
 import (
 	"fmt"
 	"os"
+
 	"github.com/spf13/cobra"
 
 	"github.com/spf13/viper"
@@ -29,12 +30,7 @@ var cfgFile string
 var rootCmd = &cobra.Command{
 	Use:   "tri",
 	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Long:  `Tri will help you get more done in less time. It's designed to be as simple as possible to help you acomplish your goals.`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	// Run: func(cmd *cobra.Command, args []string) { },
