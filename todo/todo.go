b59591896f5105713a1b4c96f755a12e366684fa
Tira Sundara <sundaralinus@gmail.com>
2021-11-02 20:28:36 +0700

    Add add command, with todo.SaveItems()

---
+++ todo/todo.go
@@ -0,0 +1,24 @@
+package todo
+
+import (
+	"encoding/json"
+	"io/ioutil"
+)
+
+type Item struct {
+	Text string
+}
+
+func SaveItems(filename string, items []Item) error {
+	b, err := json.Marshal(items)
+	if err != nil {
+		return err
+	}
+
+	err = ioutil.WriteFile(filename, b, 0644)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
